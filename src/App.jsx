4220a54e4fa818d03490e0e7ca824844244eb56e
MaccccD <Dumisanimofolo@gmail.com>
2025-09-23 20:08:50 +0200

    Nav Bar Addition Update
    -added a sticking nav bar so its easier to navigate the different pages.

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import Recommendation from './Pages/Recommendation';
 import Consulting from './Pages/Consulting';
 import "./App.css";
 import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
+import Navbar from './Components Context/Navbar';
 
 function App() {
   return (
    <BrowserRouter>
+   <Navbar/>
    <Routes>
-    <Route path='*' element = {<Home/>}/>
+    <Route path='/' element = {<Home/>}/>
     <Route path='/Compliance' element = {<Compliance/>}/>
     <Route path='/Consulting' element = {<Consulting/>}/>
     <Route path='/Recommendation' element = {<Recommendation/>}/>
+    <Route path='*' element = {<Home/>}/> 
    </Routes>
    </BrowserRouter>
   )
---
+++ src/Components Context/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+
+  return (
+    <nav>
+        <Link to={"/"} style={{display: 'flex', color: "white", fontSize: 14, gap: 50, cursor: 'pointer'}}>Home</Link>
+        <Link to={"/Compliance"} style={{display: "flex", color: "white", fontSize: 14, gap: 50, cursor:'pointer'}}>ESG Compliance</Link>
+        <Link to={"/Consulting"} style={{display: "flex", color: "white", fontSize: 14, gap: 50, cursor: "pointer"}}>Consulting</Link>
+        <Link to={"/Recommendations"} style={{display: "flex", color: "white", fontSize: 14, cursor: "pointer"}}>Recommendations</Link>
+    </nav>
+  )
+}
+
+export default Navbar
